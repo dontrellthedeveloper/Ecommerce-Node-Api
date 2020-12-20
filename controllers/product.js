bc71905d39a4daaa6f4ea6906f318fef971c01c3
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-19 23:42:25 -0800

    added ability to get total product count

--- controllers/product.js
+++ controllers/product.js
@@ -83,4 +83,9 @@ exports.list = async (req, res) => {
     }
 };
 
+exports.productsCount = async (req, res) => {
+    let total = await Product.find({}).estimatedDocumentCount().exec();
+    res.json(total);
+};
+
 
